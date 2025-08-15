e73b4083db07cde09719f97963a3ac4686e9a9ee
lucas <lucas.celp1@gmail.com>
2025-08-15 15:05:21 -0300

    ajustando repositório

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:testando/features/login/login_screen.dart';
 import 'package:testando/features/main/home_page.dart';
 import 'package:testando/features/profile/profile_screen.dart';
+import 'package:testando/features/register/register_screen.dart';
 // Remova os imports do provider daqui, não são mais necessários
 
 class Routes {
@@ -12,6 +13,7 @@ class Routes {
       // O provider será encontrado no contexto acima dela.
       LoginScreen.routeName: (context) => const LoginScreen(),
       ProfileScreen.routeName: (context) => const ProfileScreen(),
+      RegisterScreen.routeName: (context) => const RegisterScreen(),
     };
   }
 }
